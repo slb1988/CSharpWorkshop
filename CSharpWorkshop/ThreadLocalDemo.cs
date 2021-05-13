bf70c035defddf47a6092780f0c05174850279d3
laibing sun <sunlaibing88@gmail.com>
2021-05-13 10:35:08 +0800

    add ThreadStatic Local Demo;

---
+++ CSharpWorkshop/ThreadLocalDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/api/system.threading.threadlocal-1?view=net-5.0
+    /// </summary>
+    class ThreadLocalDemo
+    {
+        public static void Main()
+        {
+            Console.WriteLine("haha");
+            ThreadLocal<string> ThreadName = new ThreadLocal<string>(() =>
+            {
+                return "Thread " + Thread.CurrentThread.ManagedThreadId;
+            });
+
+            Action action = () =>
+            {
+                bool repeat = ThreadName.IsValueCreated;
+
+                Console.WriteLine("ThreadName {0} {1}", ThreadName.Value, repeat ? "(repeat)" : "");
+            };
+
+            Parallel.Invoke(action, action, action, action, action, action, action, action, action, action, action, action);
+
+            ThreadName.Dispose();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpWorkshop/ThreadStaticAttributeDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp10
+{
+    /// <summary>
+    /// https://www.c-sharpcorner.com/article/overview-of-threadstatic-attribute-in-c-sharp/
+    /// </summary>
+    class ThreadStaticAttributeDemo
+    {
+        static int StaticId;
+        [ThreadStatic]
+        static int ThreadStaticId = 2;
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //    for (int i =1; i <= 5; i++)
+        //    {
+        //        Thread newThread = new Thread(() => IncrementId());
+        //        newThread.Name = i.ToString();
+        //        newThread.Start();
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+        private static void IncrementId()
+        {
+            StaticId++;
+            ThreadStaticId++;
+            Console.WriteLine("Static ID Value in thread" + Thread.CurrentThread.Name + " is " + StaticId);
+            Console.WriteLine("Thread Static ID Value in thread" + Thread.CurrentThread.Name + " is " + ThreadStaticId);
+        }
+    }
+}
